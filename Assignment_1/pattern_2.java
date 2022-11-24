925cce0254b6b1d9499682b733f6a38d0875172f
Raviprakashpatel369 <raviprakashpatel369@gmail.com>
2022-11-24 09:35:31 +0530

    4th commit

--- Assignment_1/pattern_2.java
+++ Assignment_1/pattern_2.java
@@ -1,30 +1,12 @@
-public class Program
-{
+public class Main{
+
     public static void main(String[] args) {
-        int n=14;
-        for(int i=0;i<n;i++){
-            if(i==0||i==n-1)
+        int n=4;
+        for(int i=1;i<=n;i++){           
             for(int j=0;j<n;j++){
-                System.out.print("*");
-                
+                System.out.print(i+" ");
             }
-            else{
-            System.out.print("*");
-            for(int j=1;j<n/2-i;j++){
-                System.out.print("*");
-            }
-            int k=((2*i)-1<n-2)?((2*i)-1):(n-2);
-            
-            for(int j=0;j<k;j++){
-                System.out.print(" ");
-            }
-            System.out.print("*");
-             for(int j=1;j<n/2-i+1;j++){
-                System.out.print("*");
-            }
-            }
-            System.out.println();
-        }
-        
+		System.out.println();
+        }       
     }
 }
--- Assignment_1/pattern_3.java
+++ Assignment_1/pattern_3.java
@@ -1,29 +1,30 @@
-package in.company.main;
-
-import java.util.*;
-
-public class Main {
-	
-	public static void main(String[] args) {
-		int n=14;
-		int c=n/2+1;
-		for(int i=1;i<=c;++i) {
-			for(int k=0;k<i;++k)
-				System.out.print("*");
-			for(int k=0;k<n-(2*i);k++){
-				System.out.print(" ");
-			}
-			int p=(i>n/2)?n/2-1:i;
-			for(int k=0;k<p;++k)
-				System.out.print("*");
-			System.out.println();
-		}
-	}
-	
-	
+public class Program
+{
+    public static void main(String[] args) {
+        int n=14;
+        for(int i=0;i<n;i++){
+            if(i==0||i==n-1)
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+                
+            }
+            else{
+            System.out.print("*");
+            for(int j=1;j<n/2-i;j++){
+                System.out.print("*");
+            }
+            int k=((2*i)-1<n-2)?((2*i)-1):(n-2);
+            
+            for(int j=0;j<k;j++){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+             for(int j=1;j<n/2-i+1;j++){
+                System.out.print("*");
+            }
+            }
+            System.out.println();
+        }
+        
+    }
 }
-
-
-
-
-
